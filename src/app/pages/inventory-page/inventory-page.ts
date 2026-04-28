7dbc59c184467b9a8201873b4db35983d0b02c03
Lilian Brosset <lilian.bross85700@gmail.com>
2026-04-28 14:49:18 +0200

    Inventory

--- src/app/pages/inventory-page/inventory-page.ts
+++ src/app/pages/inventory-page/inventory-page.ts
@@ -1,9 +1,46 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component} from '@angular/core';
 
+import {Router, RouterOutlet} from '@angular/router';
+import { InventoryServices } from '../../services/inventory/inventory-services';
 @Component({
   selector: 'app-inventory-page',
   imports: [],
   templateUrl: './inventory-page.html',
   styleUrl: './inventory-page.css',
 })
-export class InventoryPage {}
+export class InventoryPage {
+  public items :any=[]
+
+  constructor(private router :Router, private inventoryServices : InventoryServices,private cd: ChangeDetectorRef) {}
+
+  ngOnInit(){
+    this.inventoryServices.getAllItem().subscribe({
+      next: data => {
+        this.items = data;
+        this.Trieby("name")
+        this.cd.detectChanges();
+      }
+    })
+  }
+  Trieby(propriete: string){
+    this.items.sort((a: any, b: any) => {
+      if (a[propriete]  === null) {
+        return 1;
+      }
+      if ( b[propriete] === null) {
+        return -1;
+      }
+      else if (typeof a[propriete] === 'string') {
+        return a[propriete].localeCompare(b[propriete]);
+      }
+      return a[propriete] - b[propriete];
+    });
+  }
+  HomePage(){
+    this.router.navigate(['/']);
+  }
+}
+
+
+
+
